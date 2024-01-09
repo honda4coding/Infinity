e2a134c60f0d196cbe3937a439f4ebf30755410e
Mohaned Ahmed <mhndahmd820@gmail.com>
2024-01-09 17:48:45 +0200

    version 2.0

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,8 +16,8 @@ const Contact: React.FC = () => {
           src="/resize-170386058519758890772.jpg" // Replace with your image URL
           alt="Description"
           objectFit="cover"
-          width={100}
-          height={100}
+          // width={100}
+          // height={100}
         />
 
         <div className={styles.conect}>
