56ba886d82611780f1a6970dec77e6de663c99ba
ahmed14420004 <ahmed142806@eng.psu.edu.eg>
2024-01-11 22:12:06 +0200

    s

--- components/home/home1.tsx
+++ components/home/home1.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+"use client";
+4;
+import React, { useState, useEffect } from "react";
 import styles from "./home1.module.scss";
 import Button from "../common/readmore";
 import Image from "next/image";
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 const Home1 = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 3000,
+     
+
+    });
+  }, []);
   function handleButtonClick(): void {}
   return (
     <div className={styles.container}>
-      <div className={styles.textContainer}>
+      <div data-aos="fade-right" className={styles.textContainer}>
         <h2>{`Infinity Marine Consultants (IMC)`}</h2>
         <p>
           {` We are a leading maritime company in EGYPT, with international network
@@ -23,7 +33,7 @@ const Home1 = () => {
           cursorPointer={true}
         />
       </div>
-      <div className={styles.imageContainer}>
+      <div data-aos="fade-left" className={styles.imageContainer}>
         {/* <Image src="/logo_sign.png" alt="Icon" width={500} height={500} /> */}
         <Image src="/INFINITY2.jpg" alt="Icon" width={500} height={500} />
       </div>
